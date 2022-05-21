049938e5b4536bb1ccd0c471df9ba210776934dc
rotem assa <rotemassa@mail.tau.ac.il>
2022-05-21 16:39:32 +0300

    created get functions for all properties of Country except president and prime minister. additionaly, have a few bugs marked on the function in TODO

--- Entities/Country.py
+++ Entities/Country.py
@@ -10,20 +10,53 @@ WIKIPEDIA_PREFIX = "https://en.wikipedia.org/wiki/"
 class Country:
     def __init__(self, name):
         self.name = name
-
-        name = name.replace(" ", "_")
-        self.url = WIKIPEDIA_PREFIX + name
+        self.url = WIKIPEDIA_PREFIX + name.replace(" ", "_")
         self.doc = lxml.html.fromstring(requests.get(self.url).content)
 
-    def set_values(self):
-        print(self.get_president_name(), self.get_prime_minister_name())
+        self.president = self.get_president()
+        self.prime_minister = self.get_prime_minister()
+
+        self.population = self.get_population()
+        self.area = self.get_area()
+        self.gov_form = self.get_gov_form()
+        self.capital = self.get_capital()
+
 
     def get_president_name(self):
         xpath = "/html/body/div[3]/div[3]/div[5]/div[1]/table[1]/tbody/tr[th/div/a/text() = 'President']/td/a/text()"
         for elem in self.doc.xpath(xpath):
-            return elem
+            return str(elem)
 
     def get_prime_minister_name(self):
         xpath = "/html/body/div[3]/div[3]/div[5]/div[1]/table[1]/tbody/tr[th/div/a/text() = 'Prime Minister']/td/a/text()"
         for elem in self.doc.xpath(xpath):
-            return elem
+            return str(elem)
+
+    def get_population(self):
+        xpath = "/html/body/div[3]/div[3]/div[5]/div[1]/table[1]/tbody/tr[th/a/text() ='Population']/following-sibling::*[1]/td/text()"
+        for elem in self.doc.xpath(xpath):
+            return str(elem)
+
+    def get_area(self):#TODO: need to check about the area being squared!
+        xpath = "/html/body/div[3]/div[3]/div[5]/div[1]/table[1]/tbody/tr[th/a/text() ='Area ']/following-sibling::*[1]/td/text()"
+        for elem in self.doc.xpath(xpath):
+            return str(elem)
+
+    def get_gov_form(self):
+        gov_forms = []
+        xpath = "/html/body/div[3]/div[3]/div[5]/div[1]/table[1]/tbody/tr[th/a/text() ='Area ']/following-sibling::*[1]/td/text()"#TODO: replace bad query!
+        for elem in self.doc.xpath(xpath):
+            gov_forms.append(str(elem))
+
+        return gov_forms
+
+    def get_capital(self):
+        xpath = "/html/body/div[3]/div[3]/div[5]/div[1]/table[1]/tbody/tr[th/text() = 'Capital']/td/a/text()"
+        for elem in self.doc.xpath(xpath):
+            return str(elem)
+
+    def get_president(self):
+        return
+
+    def get_prime_minister(self):
+        return None
