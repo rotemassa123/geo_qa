0f1cbcb32da00a31582272b15253665930070159
rotem assa <rotemassa@mail.tau.ac.il>
2022-05-21 15:30:16 +0300

    set infrastructure and xpath queries to get president and prime minister names

--- Entities/Country.py
+++ Entities/Country.py
@@ -1,5 +1,8 @@
 from Entities.PrimeMinister import PrimeMinister
 from Entities.President import President
+import requests
+import lxml.html
+import urllib
 
 WIKIPEDIA_PREFIX = "https://en.wikipedia.org/wiki/"
 
@@ -10,8 +13,17 @@ class Country:
 
         name = name.replace(" ", "_")
         self.url = WIKIPEDIA_PREFIX + name
+        self.doc = lxml.html.fromstring(requests.get(self.url).content)
 
-    @classmethod
-    def set_values(cls):
-        cls.prime_minister = PrimeMinister.set_values()
-        cls.president = President.set_values()
+    def set_values(self):
+        print(self.get_president_name(), self.get_prime_minister_name())
+
+    def get_president_name(self):
+        xpath = "/html/body/div[3]/div[3]/div[5]/div[1]/table[1]/tbody/tr[th/div/a/text() = 'President']/td/a/text()"
+        for elem in self.doc.xpath(xpath):
+            return elem
+
+    def get_prime_minister_name(self):
+        xpath = "/html/body/div[3]/div[3]/div[5]/div[1]/table[1]/tbody/tr[th/div/a/text() = 'Prime Minister']/td/a/text()"
+        for elem in self.doc.xpath(xpath):
+            return elem
